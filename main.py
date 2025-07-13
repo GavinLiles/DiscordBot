76eab01a7a5e42541f559deaa75dcb094a2eea85
Gavin Liles <gavinliles@gmail.com>
2025-07-12 19:34:38 -0500

    TestBot

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hi")
+# This example requires the 'message_content' intent.
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTM2MzcxOTk1ODY2Mzc5NDc0OA.G8gU9Z.n7BHMk-qUA9uBm5JYKLmJo2iUUvT8jtJ4zAP7k')
